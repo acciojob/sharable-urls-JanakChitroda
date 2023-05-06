8080b352b324712940b5d1fc0c0d673ad8ba0668
JanakChitroda <92598860+JanakChitroda@users.noreply.github.com>
2023-05-06 08:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// your code here
+document.getElementById("myForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var name = document.getElementById("name").value;
+  var year = document.getElementById("year").value;
+
+  var queryString = "";
+
+  if (name !== "") {
+    queryString += "name=" + encodeURIComponent(name);
+  }
+
+  if (year !== "") {
+    if (queryString !== "") {
+      queryString += "&";
+    }
+    queryString += "year=" + encodeURIComponent(year);
+  }
+
+  var updatedURL = "https://localhost:8080/";
+
+  if (queryString !== "") {
+    updatedURL += "?" + queryString;
+  }
+
+  document.getElementById("url").textContent = updatedURL;
+});
